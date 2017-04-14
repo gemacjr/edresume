0c20a310a7d3b7b55d758aa9afda3897777ea66f
Ed McCormic <edmccormic@gmail.com>
2017-04-14 15:38:42 -0600

    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'https://github.com/gemacjr/edresume/blob/master/public/resumeData.json',
+      url:'https://mysterious-ridge-83640.herokuapp.com/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
